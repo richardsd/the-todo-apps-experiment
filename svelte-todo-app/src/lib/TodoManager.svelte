9d891f0f0ba79662a7c21eb3834418241a52ccb8
Ricardo Dias <rdias@nuxeo.com>
2023-09-13 16:49:17 +0100

    Initial implementation of the todo app in svelte

---
+++ svelte-todo-app/src/lib/TodoManager.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from "svelte";
+
+  let data = [];
+
+  onMount(async () => {
+    const response = await fetch('http://localhost:3000/todos');
+    data = await response.json();
+  });
+
+</script>
+
+<h1>The Todo app experiment - Svelte edition</h1>
+
+<ul>
+  {#each data as todo}
+    <li>{todo.id} - {todo.description} - {todo.status}</li>
+  {/each}
+</ul>
---
+++ svelte-todo-app/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import TodoManager from "$lib/TodoManager.svelte";
+</script>
+
+<TodoManager />
