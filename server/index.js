52a628c1fee63a546d3193e80108534e7be0cac0
Ricardo Dias <rdias@nuxeo.com>
2023-09-13 16:48:05 +0100

    Update data and server to use cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const todos = require('../data/todos.json');
 
 const app = express();
+app.use(cors());
+
 const port = 3000;
 
 app.get('/', (req, res) => {
